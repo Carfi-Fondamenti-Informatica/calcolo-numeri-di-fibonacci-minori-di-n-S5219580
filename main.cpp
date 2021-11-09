95007da9b11c4bdefd2bca7a1af912389cb97d05
S5219580 <91791844+S5219580@users.noreply.github.com>
2021-11-09 19:51:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() {    
+   int n=0, primo=1, secondo=0, terzo=1;
+    cin >> n;
+    if (n>=1){
+        while (true){
+            if(terzo<=n){
+            cout<<terzo <<endl;
+            terzo=primo+secondo;
+            secondo= primo;
+            primo=terzo;}
+            else
+                break;
+        }
+    }
    return 0;
 }
